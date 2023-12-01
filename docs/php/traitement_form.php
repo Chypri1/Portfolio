ddf9676fdc49a0a8a5a232e1507e457b5e9eeb04
Cyprien Pennachi <cyprienpennachi@gmail.com>
2023-12-01 21:49:02 +0100

    ajout plusieurs pages et formulaire

---
+++ docs/php/traitement_form.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST["nom"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $destinataire = "votre@email.com";
+    $sujet = "Nouveau message de $nom";
+    $corpsMessage = "De : $nom\n\nEmail : $email\n\nMessage :\n$message";
+
+    // Envoi de l'e-mail
+    mail($destinataire, $sujet, $corpsMessage, "From: $email");
+
+    // Redirection après l'envoi du formulaire
+    header("Location: confirmation.html");
+    exit();
+}
+?>
